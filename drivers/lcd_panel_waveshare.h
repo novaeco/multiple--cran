db18edd2d925b6330e7b7b189ca25543f33aba0b
novaeco <novatribolnovea@gmail.com>
2025-06-07 15:06:17 +0200

    Fix duplicate display driver variable

--- drivers/lcd_panel_waveshare.h
+++ drivers/lcd_panel_waveshare.h
@@ -2,5 +2,10 @@
 #include <esp_lcd_panel_ops.h>
 #include <stdint.h>
 
-// Initialise le panneau LCD Waveshare et renvoie le handle
+/*
+ * Initialize the Waveshare LCD panel and return a handle.
+ * The DISP and LCD_RST control pins can be configured with the
+ * macros PIN_NUM_DISP and PIN_NUM_LCD_RST. Set a macro to -1 to
+ * disable handling of the corresponding pin.
+ */
 esp_lcd_panel_handle_t lcd_panel_waveshare_init(int width, int height);
